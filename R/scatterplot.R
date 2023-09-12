18148e55ab4ccf0a4c57f76659f823f3462d7c25
Grey Monroe <greymonroe@gmail.com>
2023-09-12 11:09:30 -0700

    add colvar and warnings

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -5,19 +5,32 @@
 #' @param data A data frame containing the data to be plotted. Example: iris.
 #' @param xvar A character string specifying the column name for the x-axis. Example: "Sepal.Length".
 #' @param yvar A character string specifying the column name for the y-axis. Example: "Sepal.Width".
-#' @export
+#' @param colvar A character string specifying the column name for the color of the points.
+#'               If NULL, all points will have the same color. Example: "Species".
 #'
 #' @return A ggplot2 scatterplot.
 #' @examples
-#' scatterplot(data = iris, xvar = "Sepal.Length", yvar = "Sepal.Width")
+#' scatterplot(data = iris, xvar = "Sepal.Length", yvar = "Sepal.Width", colvar = "Species")
 #'
 #' @importFrom ggplot2 ggplot aes geom_point
-
-
-scatterplot <- function(data, xvar, yvar) {
+#' @export
+scatterplot <- function(data, xvar, yvar, colvar = NULL) {
 
   require(ggplot2)
 
-  ggplot(data, aes(x = get(xvar), y = get(yvar))) +
+  # Check if the provided column names exist in the data
+  if (!xvar %in% names(data)) {
+    stop(paste("Error: Column", xvar, "not found in data."))
+  }
+
+  if (!yvar %in% names(data)) {
+    stop(paste("Error: Column", yvar, "not found in data."))
+  }
+
+  if (!is.null(colvar) && !colvar %in% names(data)) {
+    stop(paste("Error: Column", colvar, "not found in data."))
+  }
+
+  ggplot(data, aes(x = get(xvar), y = get(yvar), col = if (!is.null(colvar)) get(colvar))) +
     geom_point()
 }
